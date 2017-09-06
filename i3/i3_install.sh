70c475664ba36a3cf16bbaf358afc29b8f0725cc
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-06 16:44:50 +0300

    Update i3_install script

--- i3/i3_install.sh
+++ i3/i3_install.sh
@@ -3,6 +3,23 @@
 #i3 wm
 sudo apt -y install i3 i3blocks
 
+# i3-gaps
+sudo add-apt-repository -y ppa:aguignard/ppa
+sudo apt update
+libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev
+cd ~
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
 # Image viewer
 sudo apt -y install gpicview
 
