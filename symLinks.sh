dc982e5ceb3eb070d53c114d383f921198577642
Pavlo Kuziv <larkvincer@gmail.com>
2017-10-12 01:15:04 +0300

    add Xdefaults

--- symLinks.sh
+++ symLinks.sh
@@ -2,3 +2,4 @@ current_folder=$(pwd)
 ln -s $current_folder'/tmux.conf' ~/.tmux.conf
 ln -s $current_folder'/vim/' ~/.vim
 ln -s $current_folder'/i3/' ~/.config/i3
+ln -s $current_folder'/Xdefaults/' ~/.Xdefaults
