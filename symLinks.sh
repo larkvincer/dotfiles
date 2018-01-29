edd1ae207e731f82d8f4909884959ecdea826826
Pavlo Kuziv <pku7iv@gmail.com>
2018-01-29 13:53:31 +0200

    Add neovim to symlinks.

--- symLinks.sh
+++ symLinks.sh
@@ -1,6 +1,6 @@
 current_folder=$(pwd)
 ln -s $current_folder'/tmux.conf' ~/.tmux.conf
-ln -s $current_folder'/vim/' ~/.vim
+ln -s $current_folder'/nvim/' ~/.config/nvim
 ln -s $current_folder'/i3/' ~/.config/i3
 ln -s $current_folder'/Xdefaults' ~/.Xdefaults
 ln -s $current_folder'/nordic' /etc/profile.d/nordic.sh
